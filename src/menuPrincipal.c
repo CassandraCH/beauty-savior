2eb47eb8d0019ab2a04ef96cd7958867a1294f2e
cassandra <cassandra1d@hotmail.fr>
2020-02-13 14:07:38 +0100

    Ajout de commentaires v4 + modification des noms de variables

--- src/menuPrincipal.c
+++ src/menuPrincipal.c
@@ -29,46 +29,46 @@ extern void Init_MenuPrincipal()
     printf("Chargement Menu principal");
     
     //chargement du son
-    menu.sound = Mix_LoadWAV("sounds/menu_click.wav");
+    menu.son = Mix_LoadWAV("sounds/menu_click.wav");
 
     /* 
      * Premiere option : demarer une nouvelle partie 
      * couleur du début = rouge
      */ 
     menu.menu[0].couleur = (SDL_Color) { 0xFF, 0, 0,0 };
-    menu.menu[0].string = "Nouvelle Partie";
+    menu.menu[0].nomOption = "Nouvelle Partie";
     //Refraichissement de l'affichage de l'option
     UpdateOption( &menu.menu[0] );
-    //Position (en x et y) de l'option dans la fenetre 
-    menu.menu[0].x = (width / 2) - (menu.menu[0].width);
+    //Position (en x et y) de l'option dans la fenetre
+    menu.menu[0].x = (width / 2) - (menu.menu[0].largeur);
     menu.menu[0].y = height / (MAX_NUMBER + 1) * 1;
 
     /* 
      * Deuxieme option : chargement d'une partie 
      * couleur du début = blanc
      */
-    menu.menu[1].color = (SDL_Color) {0xFF,0xFF,0xFF,0xFF};
-    menu.menu[1].string = "Charger Partie";
+    menu.menu[1].couleur = (SDL_Color){0xFF, 0xFF, 0xFF, 0xFF};
+    menu.menu[1].nomOption = "Charger Partie";
     //Refraichissement de l'affichage de l'option
     UpdateOption(&menu.menu[1]);
     //Position (en x et y) de l'option dans la fenetre
-    menu.menu[1].x = (width/2) - (menu.menu[1].width);
+    menu.menu[1].x = (width / 2) - (menu.menu[1].largeur);
     menu.menu[1].y = height / (MAX_NUMBER + 1) * 2;
 
     /* 
      * Troisieme option : quitter le jeu 
      * couleur du début = blanc
      */
-    menu.menu[2].color = (SDL_Color) {0xFF,0xFF,0xFF,0xFF};
-    menu.menu[2].string = "Exit";
+    menu.menu[2].couleur = (SDL_Color){0xFF, 0xFF, 0xFF, 0xFF};
+    menu.menu[2].nomOption = "Exit";
     //Refraichissement de l'affichage de l'option
     UpdateOption(&menu.menu[2]);
     //Position (en x et y) de l'option dans la fenetre
-    menu.menu[2].x = (width/2) - (menu.menu[2].width);
+    menu.menu[2].x = (width / 2) - (menu.menu[2].largeur);
     menu.menu[2].y = height / (MAX_NUMBER + 1) * 3;
 
     //Option selectionnee = la premiere (nouvelle partie)
-    menu.selectedItem = 0;
+    menu.selectedOption = 0;
 }
 
 /* 
@@ -79,11 +79,10 @@ extern void UpdateOption(Options * menut)
     /* 
      * Surface tampon => utile pour parametrer la surface
      * TTF_RenderText_Blended => permet de creer une surface et de l'affiche en haute qualite 
-     * 
      */
-    SDL_Surface *tmp = TTF_RenderText_Blended(menu.font, menut->string, menut->color );
-    menut->width  = tmp->w;
-    menut->height= tmp->h;
+    SDL_Surface *tmp = TTF_RenderText_Blended(menu.font, menut->nomOption, menut->couleur);
+    menut->largeur = tmp->w;
+    menut->hauteur = tmp->h;
 
     //Liberer la memoire utilisee pour l'ancienne structure
     if( menut->texture != NULL)
@@ -93,8 +92,9 @@ extern void UpdateOption(Options * menut)
         menut->texture = NULL;
     }
 
-    //Creer la nouvelle texture
+    //Creer la nouvelle texture avec les parametres dans la surface tampon
     menut->texture = SDL_CreateTextureFromSurface( getRenderer() , tmp);
+
     //Liberer la memoire utilisee pour la surface tampon
     SDL_FreeSurface(tmp);
 }
@@ -103,19 +103,23 @@ extern void UpdateOption(Options * menut)
  * Fonction qui permer de naviguer dans le menu vers le haut
  */
 extern void MoveUp()
-{   
-	if ( menu.selectedItem - 1 >= 0)
-	{   
+{
+    //Si l'option actuellement selectionnee est differente de la premiere
+    //Si on est sur la premiere option => on ne peut pas aller sur une option au-dessus
+    if (menu.selectedOption - 1 >= 0)
+    {  
+        //Pour l'option actuelle => changer la couleur (mettre blanc)
+        menu.menu[menu.selectedOption].couleur = (SDL_Color){0xFF, 0xFF, 0xFF, 0xFF};
+        //Rafraichir l'affichage
+        UpdateOption(&menu.menu[menu.selectedOption]);
 
-        // Blanc
-		menu.menu[menu.selectedItem].color = (SDL_Color) {0xFF,0xFF,0xFF,0xFF};
-        UpdateOption(&menu.menu[menu.selectedItem]);
-        
-        menu.selectedItem--;
+        //Modifier l'option selectionnee => passe a l'option suivante
+        menu.selectedOption--;
 
-		//Rouge
-        menu.menu[menu.selectedItem].color = (SDL_Color) {0xFF,0,0,0};
-        UpdateOption(&menu.menu[menu.selectedItem]);
+        //Pour l'option sur laquelle on veut aller => changer la couleur (mettre rouge)
+        menu.menu[menu.selectedOption].couleur = (SDL_Color){0xFF, 0, 0, 0};
+        //Rafraichir l'affichage
+        UpdateOption(&menu.menu[menu.selectedOption]);
     }
 
 }
@@ -123,27 +127,36 @@ extern void MoveUp()
  * Fonction qui permer de naviguer dans le menu vers le bas
  */
 extern void MoveDown()
-{  
-	if ( menu.selectedItem  + 1 < MAX_NUMBER )
-	{
-        // Blanc
-		menu.menu[menu.selectedItem].color = (SDL_Color) {0xFF,0xFF,0xFF,0xFF};
-        UpdateOption(&menu.menu[menu.selectedItem]);
-		
-        menu.selectedItem++;
+{
+    //Si l'option actuellement selectionnee est differente de la derniere
+    //Si on est sur la derniere option => on ne peut pas aller sur une option en-dessous
+    if (menu.selectedOption + 1 < MAX_NUMBER)
+    {
+        //Pour l'option actuelle => changer la couleur (mettre blanc)
+        menu.menu[menu.selectedOption].couleur = (SDL_Color){0xFF, 0xFF, 0xFF, 0xFF};
+        //Rafraichir l'affichage
+        UpdateOption(&menu.menu[menu.selectedOption]);
 
-        // Rouge
-		menu.menu[menu.selectedItem].color = (SDL_Color) {0xFF,0,0,0};
-        UpdateOption(&menu.menu[menu.selectedItem]);
+        //Modifier l'option selectionnee => passe a l'option precedente
+        menu.selectedOption++;
+
+        //Pour l'option sur laquelle on veut aller => changer la couleur (mettre rouge)
+        menu.menu[menu.selectedOption].couleur = (SDL_Color){0xFF, 0, 0, 0};
+        //Rafraichir l'affichage
+        UpdateOption(&menu.menu[menu.selectedOption]);
     }
 }
 
-
+/*
+ * Fonction qui permet d'afficher le menu principal
+ * => afficher les differentes options
+ */
 extern void Draw_MenuPrincipal() 
 {
+    //Pour chaque option, afficher a l'ecran son rendu
 	for (int i = 0; i < MAX_NUMBER; i++)
 	{
-        SDL_Rect rect = { menu.menu[i].position.x, menu.menu[i].position.y, menu.menu[i].width, menu.menu[i].height };
+        SDL_Rect rect = {menu.menu[i].x, menu.menu[i].y, menu.menu[i].largeur, menu.menu[i].hauteur};
         SDL_RenderCopy( getRenderer(), menu.menu[i].texture , NULL, &rect);
 	}
 }
@@ -164,11 +177,11 @@ extern void Input_MenuPrincipal(SDL_Event* event)
             switch (event->key.keysym.sym)
             {
                 case SDLK_UP:
-                    Mix_PlayChannel(-1, getMenu()->sound, 0);
+                    Mix_PlayChannel(-1, getMenu()->son, 0);
                     MoveUp();
                     break;
                 case SDLK_DOWN:
-                    Mix_PlayChannel(-1,  getMenu()->sound, 0);
+                    Mix_PlayChannel(-1, getMenu()->son, 0);
                     MoveDown();
                     break;
                 case SDLK_LEFT:
