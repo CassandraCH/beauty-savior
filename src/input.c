3a8ce3f7c98f60119d74c2599f55ed4daedd92f0
cassandra <cassandra1d@hotmail.fr>
2020-03-18 21:52:43 +0100

    Ajout des commentaires et de la documentation du fichier inputs.c

--- src/input.c
+++ src/input.c
@@ -9,28 +9,42 @@
 
 /**
  * Gestion des entrées utilisateurs.
- */ 
+ */
+/**
+ * \fn extern void ProcessInput(SDL_Event * event)
+ * \brief Fonction qui gere les entrees utilisateur
+ * \details En fonction de si l'utilisateur est en cours de partie, sur le menu principal, sur le menu pause, sur le menu game over ou si le niveau vient d'etre acheve
+ * \param event evenement (entree utilisateur)
+ * \return pas de valeur de retour (void)
+*/
 extern void ProcessInput(SDL_Event * event)
 {
-
-   if (getBaseGame()->state == MENU_PRINCIPAL )
-   {
+    //Cas ou l'utilsateur est sur le menu principal
+    if (getBaseGame()->state == MENU_PRINCIPAL )
+    {
         Input_MenuPrincipal(event);
-   }
-   else if (getBaseGame()->state == IN_GAME )
-   {
-       Input_InGame(event);
-   }
-   else if ( getBaseGame()->state == PAUSE )
-   {
-       Input_MenuPause(event);
-   }
-   else if( getBaseGame()->state == LEVEL_COMPLETED )
-   {
-       Input_MenuContinue(event);
-   }
-   else if (getBaseGame()->state == GAMEOVER)
-   {
-       Input_MenuGameOver(event);
-   }
+    }
+
+    //Cas ou l'utilsateur est en cours de partie
+    else if (getBaseGame()->state == IN_GAME )
+    {
+        Input_InGame(event);
+    }
+    //Cas ou l'utilsateur est sur le menu pause
+    else if ( getBaseGame()->state == PAUSE )
+    {
+        Input_MenuPause(event);
+    }
+
+    //Cas ou l'utilsateur vient de terminer le niveau
+    else if( getBaseGame()->state == LEVEL_COMPLETED )
+    {
+        Input_MenuContinue(event);
+    }
+
+    //Cas ou l'utilsateur est sur le menu game over
+    else if (getBaseGame()->state == GAMEOVER)
+    {
+        Input_MenuGameOver(event);
+    }
 }
