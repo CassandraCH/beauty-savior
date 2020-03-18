ea90e7b4201da9da9c569c5138cc4068152ed7a1
celia-chelli <celiakabyle26@gmail.com>
2020-03-18 14:51:14 +0100

    enjeu.h

--- includes/enjeu.h
+++ includes/enjeu.h
@@ -1,3 +1,16 @@
+/**
+ * \file enjeu.h
+ * \author CALVADOS Cindy, CHAUMULON Cassandra, CHELLI Célia, OUSMANOVA Karina
+ * \version 1.0
+ * \date janvier 2020
+ * \brief Programme fait appel a la fonction input qui gere le clavier quand on est dans le jeu
+ */
+
+
+
+
+
+
 #ifndef __INGAME_H
 #define __INGAME_H
 
