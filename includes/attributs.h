c1ec1b95fdeec5f567e729af3d2bb426a7616fb2
celia-chelli <celiakabyle26@gmail.com>
2020-03-18 10:44:28 +0100

    attributs.h

--- includes/attributs.h
+++ includes/attributs.h
@@ -1,3 +1,13 @@
+/**
+ * \file 
+ * \author CALVADOS Cindy, CHAUMULON Cassandra, CHELLI Célia, OUSMANOVA Karina
+ * \version 1.0
+ * \date janvier 2020
+ * \brief Programme qui gère les attributs communs
+ */
+
+
+
 float w , h;
 float x , y;
 float vx, vy;
