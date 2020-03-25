69d5f84d790037e840a66e7a64c3189df8aa36d8
celia-chelli <celiakabyle26@gmail.com>
2020-03-25 15:52:16 +0100

    fichier constantes.h

--- includes/constantes.h
+++ includes/constantes.h
@@ -19,25 +19,38 @@
 #define FRAME_TARGET_TIME (1000/FPS)
 
 //Dimensions du niveau
-#define LARGEUR_NIVEAU 9000 /**< \def LARGEUR_NIVEAU */
-#define HAUTEUR_NIVEAU  720 /**< \def HAUTEUR_NIVEAU */
+#define LARGEUR_NIVEAU 9000
+/**< \def LARGEUR_NIVEAU */
+#define HAUTEUR_NIVEAU  720
+/**< \def HAUTEUR_NIVEAU */
 
 //Dimensions de la fenetre
-#define LARGEUR_FENETRE 1280 /**< CONSTANTS_H*/
-#define HAUTEUR_FENETRE 720 /**< CONSTANTS_H*/
+#define LARGEUR_FENETRE 1280
+/**< CONSTANTS_H*/
+#define HAUTEUR_FENETRE 720
+/**< CONSTANTS_H*/
 
-#define MENU_PRINCIPAL 0 /**< MENU_PRINCIPAL*/
-#define IN_GAME 1 /**< IN_GAME*/
-#define PAUSE 3 /**< PAUSE*/
-#define GAMEOVER 4 /**< GAMEOVER*/
-#define LEVEL_COMPLETED 5 /**< LEVEL_COMPLETED*/
-#define LOADING 6 /**< LOADING*/
+#define MENU_PRINCIPAL 0
+/**< MENU_PRINCIPAL*/
+#define IN_GAME 1
+/**< IN_GAME*/
+#define PAUSE 3
+/**< PAUSE*/
+#define GAMEOVER 4
+/**< GAMEOVER*/
+#define LEVEL_COMPLETED 5
+/**< LEVEL_COMPLETED*/
+#define LOADING 6
+/**< LOADING*/
 
 
 /// MENU PRINCIPAL
-#define NOUVELLE_PARTIE 0 /**< NOUVELLE_PARTIE*/
-#define CHARGER_PARTIE 1 /**< ChargerPartie */
-#define QUITTER_PARTIE 2 /**< QUITTER_PARTIE */
+#define NOUVELLE_PARTIE 0
+/**< NOUVELLE_PARTIE*/
+#define CHARGER_PARTIE 1
+/**< ChargerPartie */
+#define QUITTER_PARTIE 2
+/**< QUITTER_PARTIE */
 
 
 #define NIVEAU1 1
