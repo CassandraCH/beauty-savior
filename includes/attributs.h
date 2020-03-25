819b0b239a84bca8503a120d816cd92dcc0e07b2
celia-chelli <celiakabyle26@gmail.com>
2020-03-25 17:23:45 +0100

    le fichier attributs.h

--- includes/attributs.h
+++ includes/attributs.h
@@ -8,10 +8,10 @@
 
 
 
-float w , h; /** <largeur et hauteur de l'entite> */
-float x , y; /** <la position x et y>*/
-float vx, vy;/** <la velocite x et velocite y, la vitesse laquelle va l'entite dans une direction>*/
-typeEntite type; /** <le type de l'entite>*/
+float w , h; /** <largeur et hauteur de l'entite */
+float x , y; /** <la position x et y*/
+float vx, vy;/** <la velocite x et velocite y, la vitesse laquelle va l'entite dans une direction*/
+typeEntite type; /** <le type de l'entite*/
 
-bool estMort;/** <un type booleen pour savoir c'est l'entite est mort>*/
-int nb_lancer;/** <un compteur pour savoir le nb de lancer, attaque en cours>*/
+bool estMort;/** <un type booleen pour savoir c'est l'entite est mort*/
+int nb_lancer;/** <un compteur pour savoir le nb de lancer, attaque en cours*/
