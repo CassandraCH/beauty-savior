ff872caa7707b89f2cfc60337d4807ffab678d8a
celia-chelli <celiakabyle26@gmail.com>
2020-03-25 17:52:47 +0100

    le fichier menuLoad.h

--- includes/menuLoad.h
+++ includes/menuLoad.h
@@ -1,11 +1,21 @@
+/**
+ * \file menuLoad.h
+ * \author CALVADOS Cindy, CHAUMULON Cassandra, CHELLI Célia, OUSMANOVA Karina
+ * \version 1.0
+ * \date mars 2020
+ * \brief programme qui gere le menu de chargemnt du  jeu.
+ */
+
+
+
 #ifndef _MENU_LOAD_H
-#define _MENU_LOAD_H
+#define _MENU_LOAD_H/**< \def _MENU_LOAD_H*/
 
 
-extern void Init_MenuLoad();
+extern void Init_MenuLoad();/**< fonction d'initialisation de menu de chargemet*/
 
-extern void Input_MenuLoad(SDL_Event *event);
+extern void Input_MenuLoad(SDL_Event *event);/**<input de menu */
 
-extern void Init_Pause();
+extern void Init_Pause();/**< fonction d'initialisation du fichiers pause*/
 
 #endif
