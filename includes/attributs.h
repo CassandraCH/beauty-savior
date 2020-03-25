93de9eeebaaf805498cdae126f4b36e72061667e
celia-chelli <celiakabyle26@gmail.com>
2020-03-25 13:50:49 +0100

    fichier attributs.h

--- includes/attributs.h
+++ includes/attributs.h
@@ -3,15 +3,15 @@
  * \author CALVADOS Cindy, CHAUMULON Cassandra, CHELLI Célia, OUSMANOVA Karina
  * \version 1.0
  * \date janvier 2020
- * \brief Programme qui gère les attributs communs
+ * \brief Programme qui gère les attributs communs des entites
  */
 
 
 
-float w , h; /** test  */
-float x , y;
-float vx, vy;
-typeEntite type;
+float w , h; /** <largeur et hauteur de l'entite> */
+float x , y; /** <la position x et y>*/
+float vx, vy;/** <la velocite x et velocite y, la vitesse laquelle va l'entite dans une direction>*/
+typeEntite type; /** <le type de l'entite>*/
 
-bool estMort;
-int nb_lancer;
+bool estMort;/** <un type booleen pour savoir c'est l'entite est mort>*/
+int nb_lancer;/** <un compteur pour savoir le nb de lancer, attaque en cours>*/
