1e3bcd490b712cbc2ce410ad53f5a0f38c995ccb
celia-chelli <celiakabyle26@gmail.com>
2020-03-25 17:47:10 +0100

    le fichier menu_continue.h

--- includes/menu_continue.h
+++ includes/menu_continue.h
@@ -1,17 +1,28 @@
+/**
+ * \file menu_continue.h
+ * \author CALVADOS Cindy, CHAUMULON Cassandra, CHELLI Célia, OUSMANOVA Karina
+ * \version 1.0
+ * \date mars 2020
+ * \brief programme qui gere le menu durant le jeu.
+ */
+
+
+
+
 #ifndef _MENU_CONTINUE_H
-#define _MENU_CONTINUE_H
+#define _MENU_CONTINUE_H /**< \def _MENU_CONTINUE_H*/
 
 
 
-extern void Init_MenuContinue();
+extern void Init_MenuContinue();/**< l'initialisation du menu continue*/
 
 
-extern void Droite();
-extern void Gauche();
+extern void Droite();/**< fonction pour aller a droite*/
+extern void Gauche();/**< fonction pour aller a gauche*/
 
-extern void Input_MenuContinue(SDL_Event *event);
+extern void Input_MenuContinue(SDL_Event *event);/**<fonction input pour le menu continue*/
 
-extern void Nettoyer_MenuContinue();
+extern void Nettoyer_MenuContinue();/**< fonction nettoyer pour le menu continue */
 
 extern void Init_Continue();
 
