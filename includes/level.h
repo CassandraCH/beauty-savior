f634734b956bd5bcf8e5a160d58ce0c2bb1374e0
celia-chelli <celiakabyle26@gmail.com>
2020-03-25 17:10:36 +0100

    le fichier level.h

--- includes/level.h
+++ includes/level.h
@@ -3,40 +3,43 @@
  * \author CALVADOS Cindy, CHAUMULON Cassandra, CHELLI Célia, OUSMANOVA Karina
  * \version 1.0
  * \date mars 2020
- * \brief programme qui gere le debut de mises en place du passage de niveau .
+ * \brief programme qui gere le debut de mises en place du passage des niveaux .
  */
 
 
 
 #ifndef __LEVEL_H
-#define __LEVEL_H
+#define __LEVEL_H /**< \def __LEVEL_H*/
 
 #include "texture.h"
-
+/**
+* \struct level
+* \details structure gerant les niveux
+*/
 typedef struct level
 {
 
-    Texture_Manager levelTextures[5];
-    Texture_Manager layer_1;
-    Texture_Manager layer_2;
-    Texture_Manager layer_3;
-    
+    Texture_Manager levelTextures[5]; /**<levelTextures*/
+    Texture_Manager layer_1; /**<la couche 1  */
+    Texture_Manager layer_2;/**<la couche 2  */
+    Texture_Manager layer_3;/**<la couche 3  */
+
     int departX;
     int departY;
-    
-    int cameraX;
-    int cameraY;
 
-    
+    int cameraX; /**< le position x de la camera*/
+    int cameraY;/**< la position y de la camera*/
+
+
 }Level;
 
-extern Level *getLevel();
-extern void setNiveau(int niveau);
-extern void ChargerNiveau();
-extern void Affichage_Niveau();
-extern void DestructionNiveau();
+extern Level *getLevel(); /**< le niveau*/
+extern void setNiveau(int niveau);/**< le niveau correspond*/
+extern void ChargerNiveau();/**< le chargemnts des niveaux*/
+extern void Affichage_Niveau();/**< l'affichage des niveaux*/
+extern void DestructionNiveau();/**< les destruction des niveaux*/
 
-extern void afficher_textures_niveau(int niveau);
-extern void Debug_AfficherCollider();
+extern void afficher_textures_niveau(int niveau);/**<l'affichage des textures des niveaux*/
+extern void Debug_AfficherCollider();/**< le Debug_AfficherCollider*/
 
-#endif 
+#endif
