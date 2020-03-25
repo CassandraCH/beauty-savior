03da38a1901a89e17cdda51b1217480e3e2e5dc1
celia-chelli <celiakabyle26@gmail.com>
2020-03-25 14:41:04 +0100

    fichier texture.h

--- includes/texture.h
+++ includes/texture.h
@@ -1,24 +1,23 @@
 #ifndef __TEXTURE_H
-#define __TEXTURE_H
+#define __TEXTURE_H /** \def TEXTURE_H */
 
-#include "commun.h"
+#include "commun.h" /** les structures du fichier commun.h */
 
-typedef struct textures
+typedef struct textures /** structure des differentes  textures du jeu*/
 {
-    int h, w;
+    int h, w;/**<la taille , la largueur de la texture*/
     int row;
     SDL_Texture * texture;
-    
+
 }Texture_Manager;
 
 
-extern bool chargerImage(Texture_Manager * tex, const char * filename );
+extern bool chargerImage(Texture_Manager * tex, const char * filename );/**< booleen charger des images*/
 extern void dessinerFullImage(Texture_Manager * tex, int x, int y);
-
 extern void dessinerImages( Texture_Manager * tex, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_RendererFlip flip );
 extern void ChargerTexte(Texture_Manager* tex, TTF_Font * font, const char * text );
-extern void LibererRessources( Texture_Manager *tex );
-extern SDL_Texture* ChargerTexture(char * filepath);
-extern void ChargerTextureManager(Texture_Manager *tex, char * filepath);
+extern void LibererRessources( Texture_Manager *tex );/**< liberation des ressources*/
+extern SDL_Texture* ChargerTexture(char * filepath);/**< charger de texture de filepath(chemin de fichier)*/
+extern void ChargerTextureManager(Texture_Manager *tex, char * filepath);/**<charger des texture visuel  principal dans le jeu*/
 
 #endif
