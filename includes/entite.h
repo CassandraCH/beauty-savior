95ecf8482f17297982e94cb9fb1e8d63e587bcc7
celia-chelli <celiakabyle26@gmail.com>
2020-03-25 16:06:55 +0100

    fichier entite.h

--- includes/entite.h
+++ includes/entite.h
@@ -8,7 +8,7 @@
 
 
 #ifndef ENTITY_H
-#define ENTITY_H
+#define ENTITY_H /**< \def  ENTITY_H*/
 
 #include "commun.h"
 #include "listeElements.h"
@@ -17,15 +17,14 @@
 
 // extern bool UpdateBullets();
 
-extern bool UpdateBullets( typeEntite typeA, typeEntite typeB );
-extern void attaqueEnnemis();
-extern void CreerTir( typeEntite type,int width, int height, int startX, int startY );
-extern void SetNombreTir_Ennemis();
-extern void UpdateEnnemis();
-extern void collision_Decor();
+extern bool UpdateBullets( typeEntite typeA, typeEntite typeB );/**< booleen pour les UpdateBullets*/
+extern void attaqueEnnemis();/**< fonction attaque Ennemis*/
+extern void CreerTir( typeEntite type,int width, int height, int startX, int startY );/**< fonction tir avec parametres le type,selon la largueur, la taille et selon les vecteurs x et y */
+extern void SetNombreTir_Ennemis();/**< fonction les tirs ennemis */
+extern void UpdateEnnemis();/**< fonction UpdateEnnemis */
+extern void collision_Decor();/**< fonction collision de decor*/
 // extern void collision_Decor( typeEntite type, float w, float h , float * x, float * y , float *vy, bool * estSurSol );
-extern void collisionDetection();
+extern void collisionDetection();/**< les detection de collisions*/
 
 
 #endif
-
