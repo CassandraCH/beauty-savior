3d6e1216aa5dfaa1ece8dc44623d446e56709635
celia-chelli <celiakabyle26@gmail.com>
2020-03-25 15:53:40 +0100

    fichier enjeu.h

--- includes/enjeu.h
+++ includes/enjeu.h
@@ -12,13 +12,13 @@
 
 
 #ifndef __INGAME_H
-#define __INGAME_H
+#define __INGAME_H /**<__INGAME_H*/
 
 #include "commun.h"
 #include "inputs.h"
 
 
-extern void Input_InGame(SDL_Event *event);
+extern void Input_InGame(SDL_Event *event);/**< Input_InGame */
 
 
 #endif
