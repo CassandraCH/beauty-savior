5ed441877e66bd743993c90058e195928e44b411
ydnic-wolver <calvados.ci@gmail.com>
2020-04-30 09:07:01 +0200

    Fix fichier niveau 1, 2, 3

--- src/baseGame.c
+++ src/baseGame.c
@@ -169,9 +169,6 @@ extern void Rendu_Jeux()
     Afficher_ElementsListes( &items, osTex, os, 36, 51 ); 
     Afficher_ElementsListes( &items, rockTex, rock ,37, 35 ); 
     Afficher_ElementsListes( &items, treeTex, tree, 31, 49 ); 
-    
-
-    //afficher_textures_niveau(1);
 
     Debug_AfficherCollider();
   }
