aa070b96f8b31eef14aff66ec02eb0e4290ee061
CassandraCH <cassandra1d@hotmail.fr>
2020-05-03 19:10:58 +0200

    MAJ des commentaires et de la documentation du fichier inits.c

--- src/inits.c
+++ src/inits.c
@@ -11,12 +11,11 @@
 #include "baseGame.h"
 
 SDL_Renderer *rendu = NULL; /**< Structure du rendu */
-SDL_Window *fenetre = NULL; /**< Structure de la fenetre */
+SDL_Window *fenetre = NULL; /**< Structure de la fenêtre */
 
 /**
  * \fn extern SDL_Renderer* getRenderer(void)
- * \brief Fonction qui permet d'acceder a la structure du rendu
- * \details 
+ * \brief Fonction qui permet d'accéder à la structure du rendu
  * \return un pointeur sur une structure de type SDL_Renderer
 */
 extern SDL_Renderer* getRenderer(void)
@@ -26,35 +25,35 @@ extern SDL_Renderer* getRenderer(void)
 
 /**
  * \fn void Init(const char *title)
- * \brief Fonction qui permet d'initialiser 
- * \details Creation de la fenetre
+ * \brief Fonction qui permet d'initialiser la fenêtre
+ * \details Création de la fenetre
  * \details Chargement des polices, de l'audio et des entites
  * \param title chaine de caracteres
  * \return pas de valeur de retour (void)
 */
 void Init(const char *title)
 {
-    //Verifie que l'API des polices a bien ete initialise
+    //Verifie que l'API des polices a bien été initialisé
     if(TTF_Init() == -1)
     {
         fprintf(stderr, "Erreur d'initialisation de TTF_Init : %s\n", TTF_GetError());
         exit(EXIT_FAILURE);
     }
 
-    //Verifie que la bibliotheque SDL a bien ete initialisee
+    //Verifie que la bibliothèque SDL a bien été initialisée
     if( SDL_Init(SDL_INIT_EVENTS || SDL_INIT_VIDEO) != 0 )
     {
         printf("%s\n", SDL_GetError() );
         exit(EXIT_FAILURE);
     }
 
-    //Creation de la fenetre
+    //Création de la fenêtre
     fenetre = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,LARGEUR_FENETRE , HAUTEUR_FENETRE ,SDL_WINDOW_SHOWN );
 
-    //Creation du rendu
+    //Création du rendu
     rendu = SDL_CreateRenderer( fenetre , -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
 
-    //Verifie que la fenetre et le rendu ont ete cree correctements
+    //Vérifie que la fenêtre et le rendu ont été créé correctement
     if (fenetre == NULL || rendu == NULL)
     {
         printf("Erreur d'initialisation de l'ecran: %s \n", SDL_GetError() ) ;				                                                    ;
@@ -62,9 +61,9 @@ void Init(const char *title)
     }
 
     /*############### CHARGEMENT DES POLICES *###############*/
-    getMenu()->police = TTF_OpenFont("fonts/homizio.ttf", 20);
 
-    //Verifie que la police a bien ete charge pour le menu
+    getMenu()->police = TTF_OpenFont("fonts/homizio.ttf", 20);
+    //Vérifie que la police a bien été chargé pour le menu
     if(!getMenu()->police )
     {
         printf("Cannot find font file!!\n");
@@ -74,8 +73,7 @@ void Init(const char *title)
 
 
     getScores()->police = TTF_OpenFont("fonts/arial.ttf", 54);
-
-    //Verifie que la police a bien ete charge pour le score
+    //Verifie que la police a bien été chargé pour le score
     if(!getScores()->police )
     {
         printf("Cannot find font file!!\n");
@@ -84,15 +82,16 @@ void Init(const char *title)
 
 
     getNiveau()->police = TTF_OpenFont("fonts/arial.ttf", 54);
-    
-    //Verifie que la police a bien ete charge pour le niveau
+    //Verifie que la police a bien été chargé pour le niveau
     if(!getNiveau()->police )
     {
         printf("Cannot find font file!!\n");
         SDL_Quit();
     }
     
+
     getTime()->police  = TTF_OpenFont("fonts/arial.ttf", 54);
+    //Verifie que la police a bien été chargé pour le timer
     if(!getTime()->police )
     {
         printf("Cannot find font file!!\n");
@@ -100,16 +99,18 @@ void Init(const char *title)
     }
 
     getItem(0)->police  = TTF_OpenFont("fonts/arial.ttf",45);
+    //Verifie que la police a bien été chargé pour les items
     if(!getItem(0)->police )
     {
         printf("Cannot find font file!!\n");
         SDL_Quit();
     }
-
     getItem(1)->police  = getItem(0)->police;
     getItem(2)->police  = getItem(0)->police;
+
     
     getNombreVie()->police = TTF_OpenFont("fonts/arial.ttf", 54);
+    //Verifie que la police a bien été chargé pour le compteur de vies
     if(!getNombreVie()->police )
     {
         printf("Cannot find font file!!\n");
@@ -132,31 +133,29 @@ void Init(const char *title)
 
     Init_Textures();
   
-
-
     getBaseGame()->estActif = true;
 }
 
 /**
  * \fn void LibererRessources_Jeux(void)
- * \brief Fonction qui libere toutes les ressources du jeu 
- * \details Destruction des structures rendu et fenetre 
+ * \brief Fonction qui libère toutes les ressources du jeu 
+ * \details Destruction des structures rendu et fenêtre 
  * \return pas de valeur de retour (void)
 */
 void LibererRessources_Jeux(void)
-{
-
-   
-    //Suppression textures
+{ 
+    //Suppression des textures
     DestructionNiveau(); 
     Nettoyers_Textures();
 
+    //Libération des resspurces concernant le joueur
     LibererRessources( &getPlayer()->tex );
 
-    //Suppression des audios et des polices qui ont ete charges
+    //Suppression des audios qui ont été chargé
     Mix_FreeChunk(getMenu()->son);
     Mix_FreeMusic( getMenu()->bgm  );
 
+    //Suppression des polices qui ont été chargé
     TTF_CloseFont(getMenu()->police);
     TTF_CloseFont(getTime()->police);
     TTF_CloseFont(getScores()->police);
